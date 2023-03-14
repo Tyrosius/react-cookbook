c605e42550d222b13893e1d27eb0d22e9415c429
Tyrosius <florian.schoof@gmx.de>
2023-03-14 17:54:39 +0100

    Dropdown- und Burger-Menu fixed

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,53 +1,44 @@
+import { Dropdown, DropdownButton, Navbar, Nav } from "react-bootstrap"
+
 export const NavBar = ({ recipes }) => {
     console.log(recipes)
     return (
         <header className="row">
 
             {/* <!--Navbar--> */}
-            <nav className="navbar navbar-expand-lg col-md-12 col-lg-8 mx-auto">
+            <Navbar className="navbar col-md-12 col-lg-8 mx-auto" expand="lg">
                 <a className="navbar-brand" href="#"><lottie-player
                     src={`{https://assets5.lottiefiles.com/packages/lf20_ygOY09e7vS.json}`} background="transparent" speed="1"
                     style={{ width: 120 + "px", height: 120 + "px" }} loop autoplay></lottie-player></a>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="index.html">NASCHBAR</a>
-
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">BLOG</a>
-                        </li>
-                        <li className="nav-item dropdown"> {/* TODO dropdown toggle? https://react-bootstrap.github.io/components/dropdowns/ */}
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                REZEPTE
-                            </a>
-                            <ul className="dropdown-menu">
-                                {/*                     <!--durch map ersetzen-->
-*/}                    {/* <navDropDown /> */}
-                                {recipes ? recipes.map((recipe, i) => {
-                                    return (
-                                        <>
-                                            <li key={i}>{recipe.fields.rezeptName}</li>
-                                            <hr key={"divider" + i} className="dropdown-divider" />
-                                        </>
-                                    )
-                                }) : null}
-
-                                {/*<!--ende durch map ersetzen--> */}
-                            </ul>
-                        </li>
-                    </ul>
+                <Navbar.Toggle bsPrefix="navbar-toggler"
+                    aria-controls="navbarSupportedContent" />
+                {/* <Navbar.Collapse/> */}
+                <Navbar.Collapse variant="collapse navbar-collapse" id="navbarSupportedContent">
+                    <Nav variant="navbar-nav me-auto mb-2 mb-lg-0">
+                        <Nav.Item bsPrefix="nav-item">
+                            <Nav.Link bsPrefix="nav-link active" aria-current="page" href="index.html">NASCHBAR
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item bsPrefix="nav-item">
+                            <Nav.Link bsPrefix="nav-link" href="#">BLOG
+                            </Nav.Link>
+                        </Nav.Item>
+                        <DropdownButton variant="nav-item" title="Rezepte">
+                            {recipes ? recipes.map((recipe, i) => {
+                                return (
+                                    <>
+                                        <Dropdown.Item eventKey={i}>{recipe.fields.rezeptName}</Dropdown.Item>
+                                    </>
+                                )
+                            }) : null}
+                        </DropdownButton>
+                    </Nav>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Suche" aria-label="Search" />
                         <button className="btn-search" type="submit">Suchen</button>
                     </form>
-                </div>
-            </nav>
+                </Navbar.Collapse>
+            </Navbar>
         </header >)
 }
