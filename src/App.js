d7c400f339763894ed0df1ae4de36d4807948d04
Tyrosius <florian.schoof@gmx.de>
2023-04-06 10:57:26 +0200

    netlify api

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const getData = async () => {
     try {/* TODO errorhandling */
-      const entryItems = await fetch('http://localhost:8000/rezepte');
+      const entryItems = await fetch('https://fabulous-bombolone-3da24d.netlify.app/rezepte');
       const data = await entryItems.json();
 
       setRecipes(data.data);
